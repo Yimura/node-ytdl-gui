013fc1eb7afa23c8fc3e5987219ad47aec5996a8
Yimura <andreas.maerten@scarlet.be>
2020-08-14 10:40:13 +0200

    Data: Added config file

---
+++ data/config.js
@@ -0,0 +1,26 @@
+export default {
+    web: {
+        port: 8080,
+        allow_headers: [
+            'Authorization',
+            'Access-Control-Allow-Headers',
+            'Origin',
+            'Accept',
+            'Content-Type',
+            'Access-Control-Request-Method',
+            'Access-Control-Request-Headers'
+        ],
+        allow_methods: [
+            'GET',
+            'HEAD',
+            'PUT',
+            'PATCH',
+            'POST',
+            'DELETE'
+        ],
+        origins: [
+            'http://localhost:8080',
+            'http://127.0.0.1:8080'
+        ]
+    }
+}
