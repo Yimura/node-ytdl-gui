e50796251028975f696996d9354c840c97547dcb
Yimura <andreas.maerten@scarlet.be>
2020-09-19 15:35:32 +0200

    API: Added enter for better formatting

--- doc_root/api/v1/dl.js
+++ doc_root/api/v1/dl.js
@@ -95,6 +95,7 @@ export default class Download extends BasicEndpoint {
         }
 
         const url = data.get('url');
+        
         // Check if the host is allowed to be downloaded from, if not return 403
         const host = new URL(url).hostname;
         if (!this.getModule('settings').isDomainOk(host)) return request.reject(403);
