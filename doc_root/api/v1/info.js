069c7ca226b98164239f290d4e66b8871ac7d07d
Yimura <andreas.maerten@scarlet.be>
2020-09-19 15:33:24 +0200

    API: Info endpoint returns stream info about an url.

--- doc_root/api/v1/info.js
+++ doc_root/api/v1/info.js
@@ -1,4 +1,5 @@
 import BasicEndpoint from '../../../src/structures/endpoints/BasicEndpoint.js'
+import ytdl from 'youtube-dl'
 
 export default class Info extends BasicEndpoint {
     constructor(main) {
@@ -6,11 +7,28 @@ export default class Info extends BasicEndpoint {
     }
 
     /**
+     * @param {string} url
+     */
+    getInfo(url) {
+        return new Promise((resolve, reject) => {
+            ytdl.getInfo(url, (err, info) => {
+                if (err) resolve(err);
+
+                resolve(info);
+            });
+        });
+    }
+
+    /**
      * @param {Request} request
      */
     async get(request) {
-        
+        const url = request.query.get('url');
+        if (!url) return request.reject(400);
 
-        return true;
+        const host = new URL(url).hostname;
+        if (!this.getModule('settings').isDomainOk(host)) return request.reject(403);
+
+        return request.accept(await this.getInfo(url));
     }
 }
