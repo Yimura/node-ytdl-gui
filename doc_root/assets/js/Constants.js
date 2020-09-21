1eb74018ac1c5f010aad8c2ed1621dc03809b88d
Yimura <andreas.maerten@scarlet.be>
2020-09-21 22:56:05 +0200

    Site: Added CommonError Object to Constants

--- doc_root/assets/js/Constants.js
+++ doc_root/assets/js/Constants.js
@@ -1,3 +1,7 @@
+export const CommonError = {
+    'BLOCKED_HOST': 'The host you\'re trying to download from has been blocked by the server owner, contact the administrator if you think this is an issue.',
+};
+
 export const PlaylistHosts = [
     {
         host: 'www.youtube.com',
