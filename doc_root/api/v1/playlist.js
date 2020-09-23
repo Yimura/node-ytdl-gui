db6b3d70e0ae44b7ba559a40e3b966d0ff17a232
Yimura <andreas.maerten@scarlet.be>
2020-09-23 18:43:30 +0200

    API: Modified  playlist api to support multiple hosts in the future

--- doc_root/api/v1/playlist.js
+++ doc_root/api/v1/playlist.js
@@ -7,14 +7,44 @@ export default class Playlist extends BasicEndpoint {
     }
 
     /**
+     * @private
+     * @param {string} host
+     * @param {string} url
+     */
+    async _getPlaylistData(host, url) {
+        switch (host) {
+            case 'www.youtube.com': {
+                const id = new URLSearchParams(new URL(url).search).get('list');
+                if (!id) return null;
+
+                let data;
+
+                try {
+                    data = await ytpl(url);
+                } catch (e) {
+                    return {};
+                }
+
+                return data;
+            }
+            default: {
+                return {};
+            }
+        }
+    }
+
+    /**
      * @param {Request} request
      */
     async get(request) {
-        const id = request.query.get('id');
-        if (!id) return request.reject(400);
+        const url = request.query.get('url');
+        if (!url) return request.reject(400);
 
-        const playlist = await ytpl(id);
+        const host = new URL(url).hostname;
+        if (!this.getModule('settings').isDomainOk(host)) return request.reject(403);
 
-        return request.accept(playlist);
+        const data = await this._getPlaylistData(host, url);
+
+        return request.accept(data);
     }
 }
