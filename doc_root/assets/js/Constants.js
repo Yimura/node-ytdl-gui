b7d6e947339ca778a1ad759ad4c87beae84d47c3
Yimura <andreas.maerten@scarlet.be>
2020-09-23 18:40:13 +0200

    Constants: Added UNKNOWN to CommonError

--- doc_root/assets/js/Constants.js
+++ doc_root/assets/js/Constants.js
@@ -1,5 +1,6 @@
 export const CommonError = {
     'BLOCKED_HOST': 'The host you\'re trying to download from has been blocked by the server owner, contact the administrator if you think this is an issue.',
+    'UNKNOWN': 'Unknown Error Occured!'
 };
 
 export const PlaylistHosts = [
